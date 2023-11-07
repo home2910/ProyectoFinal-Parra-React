efc578bd77bbdda7b9fc2b6d59417f6258d8246a
home2910 <homero.parra2910@gmail.com>
2023-11-06 21:31:21 -0300

    Updated

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -2,7 +2,10 @@ import { FaShoppingCart } from "react-icons/fa";
 
 function CartWidget() {
   return (
+    <div className="d-flex">
     <FaShoppingCart color="white" size={20}  />
+    <p className="mx-2">0</p>
+    </div>
   )
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,16 +4,17 @@ function NavBar() {
     return (
         <nav className="d-flex justify-content-evenly align-items-center bg-secondary ">
             <div>
-                    <button className="btn btn-success mx-2">Home</button>
-                    <button className="btn btn-success mx-2">Productos</button>
-                    <button className="btn btn-success mx-2">Contactos</button>
+                <ul className="d-flex">
+                    <li> <button className="btn btn-success mx-2">Home</button> </li>
+                    <li> <button className="btn btn-success mx-2">Productos</button> </li>
+                    <li> <button className="btn btn-success mx-2">Contactos</button> </li>
+                </ul>
             </div>
             <div>
                 <h3 className="">Clothes Sell</h3>
             </div>
             <div className="d-flex">
                 <CartWidget />
-                <p className="mx-2">0</p>
                 </div>
             </nav>
           )
