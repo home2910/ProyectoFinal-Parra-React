85a5f3808687156ac92c82d4143b3511e2a5e475
home2910 <homero.parra2910@gmail.com>
2023-11-27 16:17:40 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Cart, ItemDetailContainer, ItemListContainer, NavBar } from "./components"
-
-
+import {
+  Cart,
+  ItemDetailContainer,
+  ItemListContainer,
+  NavBar,
+} from "./components"
 
 function App() {
-
-
-
   return (
     <BrowserRouter>
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<ItemListContainer greeting={"Bienvenido a mi tienda"}/>} />
-      <Route path="/category/:category" element={<ItemListContainer greeting={"Bienvenido a mi tienda"}/>}/>
-      <Route path="/item/:id" element={<ItemDetailContainer />} />
-      <Route path="/carrito" element={<Cart />} />
-    </Routes>
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={<ItemListContainer greeting={"Bienvenido a mi tienda"} />}
+        />
+        <Route
+          path="/category/:category"
+          element={<ItemListContainer greeting={"Bienvenido a mi tienda"} />}
+        />
+        <Route path="/item/:id" element={<ItemDetailContainer />} />
+        <Route path="/carrito" element={<Cart />} />
+      </Routes>
     </BrowserRouter>
   )
 }
-
 
 export default App
